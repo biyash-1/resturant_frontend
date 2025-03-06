819391742999539d3d9236ce52d8085b50100a07
biyash-1 <sthaalish39@gmail.com>
2025-03-06 23:49:35 +0545

    frontedn build uo

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+ <h1 class="text-2xl text-red-600">Burger hub boltey</h1>
+  <RouterView />
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+
+import Button from 'primevue/button';
+
+</script>
+
+<template>
+  <main>
+    
+<Button label="Danger" severity="danger" />
+  </main>
+</template>
