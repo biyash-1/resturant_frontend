eb68294fd82aad620ee2662e6ec5cafa2c28cf25
biyash-1 <sthaalish39@gmail.com>
2025-03-07 23:59:42 +0545

    app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from "../co"
 
 </script>
 
 <template>
  <h1 class="text-2xl text-red-600">Burger hub boltey</h1>
+ <Navbar></Navbar>
   <RouterView />
 </template>
 
