bf2815279df591a4d30ae3e7ce85ce54d8098cad
biyash-1 <sthaalish39@gmail.com>
2025-03-14 19:36:04 +0545

    router update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import FoodView from "../views/FoodView.vue"
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -17,6 +20,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+
+    {
+      path: '/food',
+      name: 'food',
+      component: FoodView
+    }
   ],
 })
 
