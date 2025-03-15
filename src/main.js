b0c09232cc696d54bd58b7250cb8f72cd4f57e86
biyash-1 <sthaalish39@gmail.com>
2025-03-15 22:00:49 +0545

    main file

--- src/main.js
+++ src/main.js
@@ -4,13 +4,24 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import { createPinia } from 'pinia'
 import "./assets/base.css"
+import Aura from '@primeuix/themes/aura';
+import { MotionPlugin } from '@vueuse/motion'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(MotionPlugin)
 
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.my-app-dark'
+        }
+    }
+});
+
 
 
 app.use(createPinia())
