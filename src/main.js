a5bf51eec33a4a45d031b62bb328b7617861ba9b
biyash-1 <sthaalish39@gmail.com>
2025-03-21 20:02:39 +0545

    main file

--- src/main.js
+++ src/main.js
@@ -1,30 +1,32 @@
-
-
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import { createPinia } from 'pinia'
 import "./assets/base.css"
 import Aura from '@primeuix/themes/aura';
 import { MotionPlugin } from '@vueuse/motion'
-
+import ToastService from 'primevue/toastservice';
+import persistedState from 'pinia-plugin-persistedstate';
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-app.use(MotionPlugin)
+const pinia = createPinia();
+pinia.use(persistedState);
 
+const app = createApp(App);
+
+app.use(ToastService);
+app.use(MotionPlugin);
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {
-            darkModeSelector: '.my-app-dark'
-        }
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark'
     }
+  }
 });
 
+// Use the configured pinia instance here
+app.use(pinia);
+app.use(router);
 
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
