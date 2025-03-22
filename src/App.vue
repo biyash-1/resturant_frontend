706fe7664544b0e31eeb61185b66313081b37d8d
biyash-1 <sthaalish39@gmail.com>
2025-03-22 21:27:46 +0545

    app enahncve

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 import {  RouterView } from 'vue-router'
 import Navbar from  "./components/Navbar.vue"
 
+import Toast from 'primevue/toast';
+
+
 </script>
 
 <template>
  <Navbar/>
+ <Toast />
   <RouterView />
 </template>
 
