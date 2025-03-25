5c30999037d59eeaa57f972b5821dc5af8b7e4fd
biyash-1 <sthaalish39@gmail.com>
2025-03-25 18:07:44 +0545

    app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
-import {  RouterView } from 'vue-router'
-import Navbar from  "./components/Navbar.vue"
-
+import { computed, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import Navbar from './components/Navbar.vue';
 import Toast from 'primevue/toast';
 
+const route = useRoute();
 
+const isAdminRoute = computed(() => {
+  const result = route.path.startsWith('/admin');
+  console.log('Current route:', route.path, 'isAdminRoute:', result);
+  return result;
+});
+
+// Optional: watch route changes to verify the behavior.
+watch(() => route.path, (newPath) => {
+  console.log('Route changed to:', newPath);
+});
 </script>
 
 <template>
- <Navbar/>
- <Toast />
-  <RouterView />
+  <div>
+    <!-- Navbar shows only when not on an admin route -->
+    <Navbar v-if="!isAdminRoute" />
+    <Toast />
+    <RouterView />
+  </div>
 </template>
-
