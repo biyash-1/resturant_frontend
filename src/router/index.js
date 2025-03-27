62e54c9babf4a8eec0169ce57cbc77c43aedcc50
biyash-1 <sthaalish39@gmail.com>
2025-03-27 15:59:06 +0545

    route managed

--- src/router/index.js
+++ src/router/index.js
@@ -73,7 +73,7 @@ const router = createRouter({
     },
     {
       path: '/orderconfirmation',
-      name: "orderconfirmation",
+      name: "orderconfirm",
       component: OrderConfirm,
       meta: {requiresAuth:true}
     },
